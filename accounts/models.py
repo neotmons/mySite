4118da120fa077c99b7359a9faa388acb530394e
neotmons <neotmons@gmail.com>
2017-08-23 20:28:12 +0900

    accounts profile 모델 생성/등록

--- accounts/models.py
+++ accounts/models.py
@@ -7,5 +7,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
-    phone_number = models.CharField(max_length=20))
+    phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.phone_number
